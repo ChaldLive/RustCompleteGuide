4abb906a79a113cb3957d972853160d73396062b
ChaldLive <chald.live@gmail.com>
2024-09-18 20:57:02 +0200

    Intermediate commit keepinCompleteGuide\mediag up with things

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
-use std::convert::From;
-// use std::option::Option;
-// use std::option::Option::Some;
-use std::string::{String, ToString};
-use std::vec::Vec;
-
 #[derive(Debug)]
-enum Media{
-    Book {title: String, author: String},
-    Movie{title: String, director: String},
-    Audiobook{title: String},
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
     Podcast(u32),
-    Placeholder
+    Placeholder,
 }
 impl Media {
-    fn description(&self) -> String{
+    fn description(&self) -> String {
         // if let Media::Book {title,author} = self{
         //     format!("Book:  {} {}", title, author)
         //
@@ -26,14 +20,14 @@ impl Media {
         // } else {
         //     String::from("Lazy description")
         // }
-        match self{
-            Media::Book {title,author} => {
+        match self {
+            Media::Book { title, author } => {
                 format!("Book:  {} {}", title, author)
             }
-            Media::Movie {title,director} => {
+            Media::Movie { title, director } => {
                 format!("Movie:  {} {}", title, director)
             }
-            Media::Audiobook {title} => {
+            Media::Audiobook { title } => {
                 format!("Audiobook: {}", title)
             }
             Media::Podcast(episode_number) => {
@@ -48,30 +42,26 @@ impl Media {
 
 #[derive(Debug)]
 struct Catalog {
-    items: Vec<Media>
+    items: Vec<Media>,
 }
 
-impl Catalog{
-    fn new() -> Self{
-        Catalog{ items: vec![]}
+impl Catalog {
+    fn new() -> Self {
+        Catalog { items: vec![] }
     }
-    fn add(&mut self, media: Media){
+    fn add(&mut self, media: Media) {
         self.items.push(media);
     }
 
-    fn get_by_index(&self, index: usize) -> MyIndexValidation{
+    fn get_by_index(&self, index: usize) -> Option<&Media> {
         if index < self.items.len() {
-            MyIndexValidation::ValueAtIdex(&self.items[index])
+            Some(&self.items[index])
         } else {
-            MyIndexValidation::NoValue
+            None
         }
     }
 }
-// With lifetime anotation
-enum MyIndexValidation<'a> {
-    ValueAtIdex(&'a Media),
-    NoValue
-}
+
 
 fn main() {
     let audiobook = Media::Audiobook {
@@ -88,40 +78,30 @@ fn main() {
     let podcast = Media::Podcast(10);
     let placeholder = Media::Placeholder;
 
-    let mut my_catalog = Catalog::new();
-    my_catalog.add(audiobook);
-    my_catalog.add(good_movie);
-    my_catalog.add(bad_book);
-    my_catalog.add(podcast);
-    my_catalog.add(placeholder);
-
-    /*      Important  Note on Enums
-        Working with enums forces the programmer to think in specific terms as Rust does not
-        have the idea of null, nil or undefined.
-        Instead, addressing e.g. a value out of index in the catalog, yields a standard
-        language built in value  of enum Option. Containing two values Some or none.
-        Some means you have something, none indicating out of range problem, in this particular
-        case.
-     */
-    // match my_catalog.items.get(0) {
-    //     Option::Some(value) =>{
-    //         println!("Item: {:#?}",value)
-    //     }
-    //     Option::None =>{
-    //         println!("Nothing at that specific index")
-    //     }
-    // }
+    let mut catalog = Catalog::new();
+    catalog.add(audiobook);
+    catalog.add(good_movie);
+    catalog.add(bad_book);
+    catalog.add(podcast);
+    catalog.add(placeholder);
 
 
-    // let item = my_catalog.get_by_index(40);
+    let item = catalog.get_by_index(40);
+    if let Some(value) = catalog.get_by_index(9999) {
+        println!("Item in pattern match {:#?}", value)
+    } else {
+        println!("No value found whatsoever")
+    }
+
+
     // println!("{:#?}", my_catalog);
 
-    match my_catalog.get_by_index(0){
-        MyIndexValidation::ValueAtIdex(media) => {
-            print!("{:#?}", media);
+    match catalog.get_by_index(0) {
+        Some(value) => {
+            print!("{:#?}", value)
         }
-        MyIndexValidation::NoValue => {
-            println!("NoValue available here.");
+        None => {
+            println!("NoValue available here.")
         }
     }
 }
