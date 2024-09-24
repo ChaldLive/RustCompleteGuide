9a7ff77a985115fcecc53512bf09ba2c65b83b2b
ChaldLive <chald.live@gmail.com>
2024-09-24 16:56:27 +0200

    Modules defined and understoood here

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,7 @@
-#[derive(Debug)]
-enum Media {
-    Book { title: String, author: String },
-    Movie { title: String, director: String },
-    Audiobook { title: String },
-    Podcast(u32),
-    Placeholder,
-}
-impl Media {
-    fn description(&self) -> String {
-        // if let Media::Book {title,author} = self{
-        //     format!("Book:  {} {}", title, author)
-        //
-        // } else if let Media::Movie {title,director} = self{1
-        //     format!("Movie {} {}", title, director)
-        //
-        // }
-        // else if let Media::Audiobook {title} = Self {
-        //     format!("{}", title)
-        // } else {
-        //     String::from("Lazy description")
-        // }
-        match self {
-            Media::Book { title, author } => {
-                format!("Book:  {} {}", title, author)
-            }
-            Media::Movie { title, director } => {
-                format!("Movie:  {} {}", title, director)
-            }
-            Media::Audiobook { title } => {
-                format!("Audiobook: {}", title)
-            }
-            Media::Podcast(episode_number) => {
-                format!("Podcast episode: {}", episode_number)
-            }
-            Media::Placeholder => {
-                "Placeholder".to_string()
-            }
-        }
-    }
-}
+mod content;
 
-#[derive(Debug)]
-struct Catalog {
-    items: Vec<Media>,
-}
-
-impl Catalog {
-    fn new() -> Self {
-        Catalog { items: vec![] }
-    }
-    fn add(&mut self, media: Media) {
-        self.items.push(media);
-    }
-
-    fn get_by_index(&self, index: usize) -> Option<&Media> {
-        if index < self.items.len() {
-            Some(&self.items[index])
-        } else {
-            None
-        }
-    }
-}
+use content::media::Media;
+use content::catalog::Catalog;
 
 
 fn main() {
